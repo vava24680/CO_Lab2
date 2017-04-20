ad112720367ae58c2a46f6769a366981408d5ec6
Leo Wang <star901799@gmail.com>
2017-04-20 14:43:56 +0800

    Update Adder.v
    Main function done!!

--- code/Adder.v
+++ code/Adder.v
@@ -10,10 +10,10 @@
 //--------------------------------------------------------------------------------
 
 module Adder(
-    src1_i,
+    	src1_i,
 	src2_i,
 	sum_o
-	);
+);
 
 //I/O ports
 input  [32-1:0]  src1_i;
@@ -26,6 +26,7 @@ wire    [32-1:0]	 sum_o;
 //Parameter
 
 //Main function
+assign sum_o = src1_i + src2_i;
 
 endmodule
 
