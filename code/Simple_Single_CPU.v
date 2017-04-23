daf1a5f59d87d34165147792a105b65bb885a606
vava24680 <9203700@gmail.com>
2017-04-23 11:33:55 +0800

    Simple_Single_CPU finished 85%

--- code/Simple_Single_CPU.v
+++ code/Simple_Single_CPU.v
@@ -21,10 +21,11 @@ input         rst_i;
 /*For PC Module*/
 wire [32-1:0] pc_number;
 wire [32-1:0] pc_number_next;
-wire [32-1:0] pc_number_start;
+wire [32-1:0] pc_number_in;
 /*For IM Module*/
 wire [32-1:0] instruction_o;
 /*For Reg_File Module*/
+wire [5-1:0]  WriteReg;
 wire [32-1:0] RSdata_o;
 wire [32-1:0] RTdata_o;
 /*For Decoder Module*/
@@ -49,19 +50,22 @@ wire [32-1:0] SL_32_data_o;
 assign	ALU_src_1=RSdata_o;
 //Greate componentes
 ProgramCounter PC(
+		//Done
         .clk_i(clk_i),
 	    .rst_i (rst_i),
-	    .pc_in_i(pc_number_start),
+	    .pc_in_i(pc_number_in),
 	    .pc_out_o(pc_number)
 	    );
 
 Adder Adder1(
+		//Done
         .src1_i(pc_number),
 	    .src2_i(32'd4),
 	    .sum_o(pc_number_next)
 	    );
 
 Instr_Memory IM(
+		//Done
         .pc_addr_i(pc_number),
 	    .instr_o(instruction_o)
 	    );
@@ -69,16 +73,17 @@ Instr_Memory IM(
 MUX_2to1 #(.size(5)) Mux_Write_Reg(
         .data0_i(),
         .data1_i(),
-        .select_i(),
-        .data_o()
+        .select_i(RegDst_o),
+        .data_o(WriteReg)
         );
 
 Reg_File RF(
+		//Done
         .clk_i(clk_i),
 	    .rst_i(rst_i),
         .RSaddr_i(instruction_o[25:21]),
         .RTaddr_i(instruction_o[20:16]),
-        .RDaddr_i(),
+        .RDaddr_i(WriteReg),
         .RDdata_i(result_o),
         .RegWrite_i (RegWrite_o),
         .RSdata_o(RSdata_o),
@@ -86,6 +91,7 @@ Reg_File RF(
         );
 
 Decoder Decoder(
+		//Done
         .instr_op_i(instruction_o[31:26]),
 	    .RegWrite_o(RegWrite_o),
 	    .ALU_op_o(ALU_op_o),
@@ -95,17 +101,20 @@ Decoder Decoder(
 	    );
 
 ALU_Ctrl AC(
+		//Done
         .funct_i(instruction_o[5:0]),
         .ALUOp_i(ALU_op_o),
         .ALUCtrl_o(ALUCtrl_o)
         );
 
 Sign_Extend SE(
+		//Done
         .data_i(instruction_o[16-1:0]),
         .data_o(SE_data_o)
         );
 
 MUX_2to1 #(.size(32)) Mux_ALUSrc(
+		//Done
         .data0_i(RTdata_o),
         .data1_i(SE_data_o),
         .select_i(ALUSrc_o),
@@ -113,6 +122,7 @@ MUX_2to1 #(.size(32)) Mux_ALUSrc(
         );
 
 ALU ALU(
+		//Done
         .src1_i(ALU_src_1),
 	    .src2_i(ALU_src_2),
 	    .ctrl_i(ALUCtrl_o),
@@ -121,21 +131,24 @@ ALU ALU(
 	    );
 
 Adder Adder2(
+		//Done
         .src1_i(pc_number_next),
 	    .src2_i(SL_32_data_o),
 	    .sum_o(Adder2_result)
 	    );
 
 Shift_Left_Two_32 Shifter(
+		//Done
         .data_i(instruction_o[16-1:0]),
         .data_o(SL_32_data_o)
         );
 
 MUX_2to1 #(.size(32)) Mux_PC_Source(
+		//Done
         .data0_i(pc_number_next),
         .data1_i(Adder2_result),
         .select_i(Branch_o && zero_o),
-        .data_o()
+        .data_o(pc_number_in)
         );
 
 endmodule
